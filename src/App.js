2b9c3c63a56067e88ee2a750073641564493d590
LawrenceJ1 <lawrencejuottawa@gmail.com>
2022-02-23 13:42:53 -0500

    add numbered-title class

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      00 Home 01 Destination 02 Crew 03 Technology So, you want to travel to
-      Space Let’s face it; if you want to go to space, you might as well
-      genuinely go to outer space and not hover kind of on the edge of it. Well
-      sit back, and relax because we’ll give you a truly out of this world
-      experience! Explore
+      <nav className="flex">
+        <h2 className="numbered-title">
+          <span>00</span> Home
+        </h2>
+        <h2 className="numbered-title">
+          <span>01</span> Destination
+        </h2>
+        <h2 className="numbered-title">
+          <span>02</span>Crew
+        </h2>
+        <h2 className="numbered-title">
+          <span>03</span>
+          Technology
+        </h2>
+      </nav>
+      So, you want to travel to Space Let’s face it; if you want to go to space,
+      you might as well genuinely go to outer space and not hover kind of on the
+      edge of it. Well sit back, and relax because we’ll give you a truly out of
+      this world experience! Explore
     </div>
   );
 }
