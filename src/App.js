b83b9753864ae34ecd5e7487ae3f3db04f7d3224
LawrenceJ1 <lawrencejuottawa@gmail.com>
2022-02-23 14:16:55 -0500

    add .large-button class

--- src/App.js
+++ src/App.js
@@ -21,7 +21,15 @@ function App() {
       So, you want to travel to Space Let’s face it; if you want to go to space,
       you might as well genuinely go to outer space and not hover kind of on the
       edge of it. Well sit back, and relax because we’ll give you a truly out of
-      this world experience! Explore
+      this world experience!
+      <div className="flex">
+        <a
+          href="#"
+          className="large-button bg-white fs-600 text-dark ff-serif uppercase"
+        >
+          Explore
+        </a>
+      </div>
     </div>
   );
 }
