ca14a69d9b2a3648fdfda552b0018354f4e3364e
LawrenceJ1 <lawrencejuottawa@gmail.com>
2022-02-23 14:39:40 -0500

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import "./App.css";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="container">
-      <nav>
-        <ul className="flex primary-navigation underline-indicators">
-          <li className="active">
-            <a className="uppercase text-white letter-spacing-2">
-              <span>00</span> Home
-            </a>
-          </li>
-          <li>
-            <a className="uppercase text-white letter-spacing-2">
-              <span>01</span> Destination
-            </a>
-          </li>
-          <li>
-            <a className="uppercase text-white letter-spacing-2">
-              <span>02</span>Crew
-            </a>
-          </li>
-          <a className="uppercase text-white letter-spacing-2">
-            <span>03</span>
-            Technology
-          </a>
-        </ul>
-      </nav>
+      <NavBar />
       So, you want to travel to Space Let’s face it; if you want to go to space,
       you might as well genuinely go to outer space and not hover kind of on the
       edge of it. Well sit back, and relax because we’ll give you a truly out of
