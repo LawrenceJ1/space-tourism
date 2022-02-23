a5a61aee19ba2c72ba295d67f9561881126c086d
LawrenceJ1 <lawrencejuottawa@gmail.com>
2022-02-23 14:42:00 -0500

    add fragment wrapper

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import "./App.css";
 import NavBar from "./NavBar";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <div className="container">
+    <Fragment>
       <NavBar />
-      So, you want to travel to Space Let’s face it; if you want to go to space,
-      you might as well genuinely go to outer space and not hover kind of on the
-      edge of it. Well sit back, and relax because we’ll give you a truly out of
-      this world experience!
-      <div className="flex">
-        <a
-          href="#"
-          className="large-button bg-white fs-600 text-dark ff-serif uppercase"
-        >
-          Explore
-        </a>
+      <div className="container">
+        So, you want to travel to Space Let’s face it; if you want to go to
+        space, you might as well genuinely go to outer space and not hover kind
+        of on the edge of it. Well sit back, and relax because we’ll give you a
+        truly out of this world experience!
+        <div className="flex">
+          <a
+            href="#"
+            className="large-button bg-white fs-600 text-dark ff-serif uppercase"
+          >
+            Explore
+          </a>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
