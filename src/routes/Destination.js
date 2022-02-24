25ec178ac2123662f4027cf7d1994939e579cd04
LawrenceJ1 <lawrencejuottawa@gmail.com>
2022-02-24 14:43:54 -0500

    add background images'

--- src/routes/Destination.js
+++ src/routes/Destination.js
@@ -15,6 +15,21 @@ const Destination = () => {
 
   return (
     <Fragment>
+      <img
+        src="/assets/destination/background-destination-desktop.jpg"
+        alt="background"
+        className={styles.bg1}
+      />
+      <img
+        src="/assets/destination/background-destination-tablet.jpg"
+        alt="background"
+        className={styles.bg2}
+      />
+      <img
+        src="/assets/destination/background-destination-mobile.jpg"
+        alt="background"
+        className={styles.bg3}
+      />
       <NavBar />
       <main className="grid-container">
         <div className="numbered-title">
@@ -22,25 +37,45 @@ const Destination = () => {
           {data.destinations.map((value, index) => (
             <img
               src={value.images.png}
-              key={index}
+              key={Math.random() * 1000000}
               data-visible={index == cur ? "true" : "false"}
               className={styles.planets}
             />
           ))}
         </div>
-        <div className="underline-indicators flex">
+        <div>
+          <div className="underline-indicators flex">
+            {data.destinations.map((value, index) => (
+              <span
+                key={Math.random() * 1000000}
+                data-index={index}
+                aria-selected={index == cur ? "true" : "false"}
+                className={
+                  "letter-spacing-2 uppercase text-white " + styles.destinations
+                }
+                onClick={clickHandler}
+              >
+                {value.name}
+              </span>
+            ))}
+          </div>
           {data.destinations.map((value, index) => (
-            <span
-              key={index}
-              data-index={index}
-              aria-selected={index == cur ? "true" : "false"}
-              className={
-                "letter-spacing-2 uppercase text-white " + styles.destinations
-              }
-              onClick={clickHandler}
+            <h1
+              key={Math.random() * 1000000}
+              data-visible={index == cur ? "true" : "false"}
+              className={"fs-900 ff-serif uppercase " + styles.name}
             >
               {value.name}
-            </span>
+            </h1>
+          ))}
+          {data.destinations.map((value, index) => (
+            <p
+              key={Math.random() * 1000000}
+              data-visible={index == cur ? "true" : "false"}
+              className={"text-accent " + styles.description}
+            >
+              {value.description}
+            </p>
           ))}
         </div>
       </main>
