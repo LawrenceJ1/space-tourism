446e79eb9ee7267d086aef8fe7c6ba809b3cc34e
LawrenceJ1 <lawrencejuottawa@gmail.com>
2022-02-24 14:11:44 -0500

    start destinations page

--- src/routes/Destination.js
+++ src/routes/Destination.js
@@ -1,13 +1,48 @@
 import { Fragment } from "react";
 import NavBar from "../NavBar";
-import "./Destination.module.css";
+import styles from "./Destination.module.css";
 import data from "../data.json";
+
+let list = new Array(data.destinations.length);
+for (let i = 0; i < list.length; i++) {
+  if (i === 0) {
+    list[0] = 1;
+  } else {
+    list[i] = 0;
+  }
+}
 
 const Destination = () => {
   console.log(data.destinations);
+  console.log(list);
   return (
     <Fragment>
       <NavBar />
+      <main className="container-grid">
+        <div className="numbered-title">
+          <span>01</span> Pick your destination
+          {data.destinations.map((value, index) => (
+            <img
+              src={value.images.png}
+              key={index}
+              data-visible={list[index]}
+              className={styles.planets}
+            />
+          ))}
+        </div>
+        <div className="underline-indicators">
+          {data.destinations.map((value, index) => (
+            <span
+              key={index}
+              data-index={index}
+              aria-selected={list[index] ? "true" : "false"}
+              className={styles.destinations}
+            >
+              {value.name}
+            </span>
+          ))}
+        </div>
+      </main>
     </Fragment>
   );
 };
