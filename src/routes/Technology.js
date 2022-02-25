8e041f8a840cfdbef846b190550823b700542e96
LawrenceJ1 <lawrencejuottawa@gmail.com>
2022-02-25 11:34:42 -0500

    start technology page

--- src/routes/Technology.js
+++ src/routes/Technology.js
@@ -1,11 +1,54 @@
 import NavBar from "../NavBar";
-import "./Technology.module.css";
+import styles from "./Technology.module.css";
+import data from "../data.json";
+import { Fragment, useState } from "react";
 
 const Technology = () => {
+  const [cur, setCur] = useState(0);
+
+  const clickHandler = (e) => {
+    let selected = e.target.attributes["data-index"].value;
+    if (cur != selected) {
+      setCur(selected);
+    }
+  };
+
   return (
-    <div>
+    <Fragment>
+      <img
+        src="/assets/technology/background-technology-desktop.jpg"
+        alt="background"
+        className={styles.bg1}
+      />
+      <img
+        src="/assets/technology/background-technology-tablet.jpg"
+        alt="background"
+        className={styles.bg2}
+      />
+      <img
+        src="/assets/technology/background-technology-mobile.jpg"
+        alt="background"
+        className={styles.bg3}
+      />
       <NavBar />
-    </div>
+      <main className={"grid-container flow " + styles.con}>
+        <h1 className="numbered-title">
+          <span aria-hidden="true">03</span> Space Launch 101
+        </h1>
+        <div className={"numbered-indicators flex " + styles.tabs}>
+          {data.technology.map((value, index) => (
+            <button
+              key={Math.random() * 1000000}
+              data-index={index}
+              aria-selected={index == cur ? "true" : "false"}
+              onClick={clickHandler}
+            >
+              {index}
+            </button>
+          ))}
+        </div>
+      </main>
+    </Fragment>
   );
 };
 
