d1bd5fbb7d28a7cd0c3a48dff4321a4c1d8535a5
LawrenceJ1 <lawrencejuottawa@gmail.com>
2022-02-25 13:30:57 -0500

    use hashrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./routes/App";
 import Destination from "./routes/Destination";
@@ -8,13 +8,13 @@ import Crew from "./routes/Crew";
 import Technology from "./routes/Technology";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="destination" element={<Destination />} />
       <Route path="crew" element={<Crew />} />
       <Route path="technology" element={<Technology />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
